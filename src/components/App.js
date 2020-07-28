c1a2f66c3694e021b2b7ce6ccfe2db58d7fd3503
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-27 21:50:51 -0700

    Add components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  );
+}
+
+export default App;
