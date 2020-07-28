23ebf0d417fc7175d35a71633060feebc284573b
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-28 15:14:11 -0700

    set up base for displaying products list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,7 +9,6 @@ class ProductList extends Component {
     products: storeProducts
   };
   render() {
-    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
@@ -18,7 +17,9 @@ class ProductList extends Component {
             <div className="row">
               <ProductConsumer>
                 { value => {
-                  return <h1>{value}</h1>;
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  })
                 }}
               </ProductConsumer>
             </div>
--- src/context.js
+++ src/context.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { storeProducts, detailProduct } from './data';
 
 const ProductContext = React.createContext();
 //Provider
 //Consumer
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  }
+  handleDetail = () => {
+    console.log('hello from details');
+  }
+  addToCart = () => {
+    console.log('hello from add to cart');
+  }
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider 
+        value = {{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
