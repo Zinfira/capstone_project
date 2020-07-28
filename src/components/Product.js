8c066d56084bb6e6767deaead120349270f67f86
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-28 16:03:29 -0700

    Add card footer to display title and price of the product

--- src/components/Product.js
+++ src/components/Product.js
@@ -30,6 +30,15 @@ class Product extends Component {
             )}
             </button>
           </div>
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">
+              {title}
+            </p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
+          </div>
         </div>
       </ProductWrapper>
     );
