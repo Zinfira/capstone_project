2a000ca7b3200246313daa0b80b92937ac98abd7
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-28 15:54:46 -0700

    Add button to add in cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,6 +13,22 @@ class Product extends Component {
             <Link to='/details'>
               <img src={img} alt="product" className="card-img-top" />
             </Link>
+            <button 
+              className="cart-btn" 
+              disabled={inCart ? true : false} 
+              onClick={() => {
+                console.log('added to the cart');
+              }} 
+            >
+            {inCart ? (
+              <p className="text-capitalize mb-0" disabled>
+                {" "}
+                in cart
+              </p>
+            ) : (
+              <i className="fas fa-cart-plus"/>
+            )}
+            </button>
           </div>
         </div>
       </ProductWrapper>
